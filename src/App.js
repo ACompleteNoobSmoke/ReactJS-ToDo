501eb80c39802128f85c93740731398d8792eff0
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-04-20 03:38:54 -0500

    Worked on functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import Todo from './components/Todo';
+import TodoList from './components/TodoList';
 
 
 function App() {
   const [todos, setTodos] = useState([]);
+
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>React To Do List</p>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos = {todos} />
       </header>
     </div>
   );
